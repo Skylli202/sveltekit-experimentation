2ea7d4adc42eded60397285e672cd14f4a98c45d
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-11-12 03:49:31 -0800

    type Payment

---
+++ src/routes/data-table/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" context="module">
+	export type Payment = {
+		id: string;
+		amount: number;
+		status: 'pending' | 'processing' | 'success' | 'failed';
+		email: string;
+	};
+</script>
