aa1532b3800215982483fe37475436c65eb0e42f
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2023-11-12 15:09:55 -0800

    Filtering

--- src/routes/data-table/data-table.svelte
+++ src/routes/data-table/data-table.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
 	import type { Payment } from './+page.svelte';
-	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
-	import { addPagination, addSortBy } from 'svelte-headless-table/plugins';
+	import {
+		createTable,
+		Render,
+		Subscribe,
+		createRender,
+	} from 'svelte-headless-table';
+	import {
+		addPagination,
+		addSortBy,
+		addTableFilter,
+	} from 'svelte-headless-table/plugins';
 	import { readable } from 'svelte/store';
 	import { ArrowUpDown } from 'lucide-svelte';
 
 	import * as Table from '$lib/components/ui/table';
 	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import DataTableActions from './data-table-actions.svelte';
 
 	export let data: Payment[];
@@ -15,11 +25,23 @@
 	const table = createTable(readableData, {
 		page: addPagination(),
 		sort: addSortBy(),
+		filter: addTableFilter({
+			fn: ({ filterValue, value }) =>
+				value.toLowerCase().includes(filterValue.toLowerCase()),
+		}),
 	});
 
 	const columns = table.createColumns([
-		table.column({ accessor: 'id', header: 'ID', plugins: { sort: { disable: true } } }),
-		table.column({ accessor: 'status', header: 'Status', plugins: { sort: { disable: true } } }),
+		table.column({
+			accessor: 'id',
+			header: 'ID',
+			plugins: { sort: { disable: true }, filter: { exclude: true } },
+		}),
+		table.column({
+			accessor: 'status',
+			header: 'Status',
+			plugins: { sort: { disable: true }, filter: { exclude: true } },
+		}),
 		table.column({ accessor: 'email', header: 'Email' }),
 		table.column({
 			accessor: 'amount',
@@ -34,7 +56,7 @@
 
 				return formatted;
 			},
-			plugins: { sort: { disable: true } },
+			plugins: { sort: { disable: true }, filter: { exclude: true } },
 		}),
 		table.column({
 			accessor: ({ id }) => id,
@@ -42,7 +64,7 @@
 			cell: ({ value }) => {
 				return createRender(DataTableActions, { id: value });
 			},
-			plugins: { sort: { disable: true } },
+			plugins: { sort: { disable: true }, filter: { exclude: true } },
 		}),
 	]);
 
@@ -50,80 +72,96 @@
 		table.createViewModel(columns);
 
 	const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page;
+	const { filterValue } = pluginStates.filter;
 </script>
 
-<div class="rounded-md border">
-	<Table.Root {...$tableAttrs}>
-		<Table.Header>
-			{#each $headerRows as headerRow}
-				<Subscribe rowAttrs={headerRow.attrs()}>
-					<Table.Row>
-						{#each headerRow.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
-								<Table.Head {...attrs}>
-									{#if cell.id === 'amount'}
-										<div class="text-right">
+<div>
+	<div class="flex items-center py-4">
+		<Input
+			class="max-w-sm"
+			placeholder="Filter emails..."
+			type="text"
+			bind:value={$filterValue}
+		/>
+	</div>
+	<div class="rounded-md border">
+		<Table.Root {...$tableAttrs}>
+			<Table.Header>
+				{#each $headerRows as headerRow}
+					<Subscribe rowAttrs={headerRow.attrs()}>
+						<Table.Row>
+							{#each headerRow.cells as cell (cell.id)}
+								<Subscribe
+									attrs={cell.attrs()}
+									let:attrs
+									props={cell.props()}
+									let:props
+								>
+									<Table.Head {...attrs}>
+										{#if cell.id === 'amount'}
+											<div class="text-right">
+												<Render of={cell.render()} />
+											</div>
+										{:else if cell.id === 'email'}
+											<Button variant="ghost" on:click={props.sort.toggle}>
+												<Render of={cell.render()} />
+												<ArrowUpDown class="ml-2 h-4 w-4" />
+											</Button>
+										{:else}
 											<Render of={cell.render()} />
-										</div>
-									{:else if cell.id === 'email'}
-										<Button variant="ghost" on:click={props.sort.toggle}>
-											<Render of={cell.render()} />
-											<ArrowUpDown class="ml-2 h-4 w-4" />
-										</Button>
-									{:else}
-										<Render of={cell.render()} />
-									{/if}
-								</Table.Head>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Header>
+										{/if}
+									</Table.Head>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Header>
 
-		<Table.Body {...$tableBodyAttrs}>
-			{#each $pageRows as row (row.id)}
-				<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-					<Table.Row {...rowAttrs}>
-						{#each row.cells as cell (cell.id)}
-							<Subscribe attrs={cell.attrs()} let:attrs>
-								<Table.Cell {...attrs}>
-									{#if cell.id === 'amount'}
-										<div class="text-right font-medium">
+			<Table.Body {...$tableBodyAttrs}>
+				{#each $pageRows as row (row.id)}
+					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+						<Table.Row {...rowAttrs}>
+							{#each row.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs>
+									<Table.Cell {...attrs}>
+										{#if cell.id === 'amount'}
+											<div class="text-right font-medium">
+												<Render of={cell.render()} />
+											</div>
+										{:else if cell.id === 'status'}
+											<div class="capitalize">
+												<Render of={cell.render()} />
+											</div>
+										{:else}
 											<Render of={cell.render()} />
-										</div>
-									{:else if cell.id === 'status'}
-										<div class="capitalize">
-											<Render of={cell.render()} />
-										</div>
-									{:else}
-										<Render of={cell.render()} />
-									{/if}
-								</Table.Cell>
-							</Subscribe>
-						{/each}
-					</Table.Row>
-				</Subscribe>
-			{/each}
-		</Table.Body>
-	</Table.Root>
-</div>
+										{/if}
+									</Table.Cell>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
 
-<div class="flex items-center justify-end space-x-2 py-4">
-	<Button
-		variant="outline"
-		size="sm"
-		on:click={() => ($pageIndex = $pageIndex - 1)}
-		disabled={!$hasPreviousPage}
-	>
-		Previous
-	</Button>
-	<Button
-		variant="outline"
-		size="sm"
-		on:click={() => ($pageIndex = $pageIndex + 1)}
-		disabled={!$hasNextPage}
-	>
-		Next
-	</Button>
+	<div class="flex items-center justify-end space-x-2 py-4">
+		<Button
+			variant="outline"
+			size="sm"
+			on:click={() => ($pageIndex = $pageIndex - 1)}
+			disabled={!$hasPreviousPage}
+		>
+			Previous
+		</Button>
+		<Button
+			variant="outline"
+			size="sm"
+			on:click={() => ($pageIndex = $pageIndex + 1)}
+			disabled={!$hasNextPage}
+		>
+			Next
+		</Button>
+	</div>
 </div>
